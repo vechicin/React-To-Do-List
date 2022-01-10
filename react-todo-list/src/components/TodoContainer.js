76b3b1bf0c7206dfb686a2bb1708fa174a94175d
Santiago Velosa <santiagovelosa@gmail.com>
2022-01-10 15:50:55 -0500

    Add styling to project

--- react-todo-list/src/components/TodoContainer.js
+++ react-todo-list/src/components/TodoContainer.js
@@ -2,7 +2,7 @@ import React from "react";
 import TodosList from "./TodosList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 class TodoContainer extends React.Component {
   state = {
@@ -64,14 +64,16 @@ class TodoContainer extends React.Component {
 
   render() {
     return (
-      <div>
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodosList
-        todos={this.state.todos}
-        handleChangeProps={this.handleChange}
-        deleteTodoProps={this.delTodo}
-        />
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+          />
+        </div>
       </div>
     );
   }
